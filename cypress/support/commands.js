fc9a367ff121bc6c563e6b228eb4d9cd13c8b551
mmaaraujo7 <mmaaraujo7@gmail.com>
2020-06-30 11:31:56 -0300

    fix: change command cadastrarUsuario to work on any route

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,25 @@
 
 
 Cypress.Commands.add("cadastrarUsuario", (nome, email, senha) => {
-    cy.request("POST", `https://seubarriga.wcaquino.me/cadastrarUsuario?nome=${nome}&email=${email}&senha=${senha}`)
-    .then((res) => {
-        expect(res.status).to.eq(200);
-    });
+    cy.window().then((win) => {
+        cy.log(win.location.href);
+        var dorme = win.location.href.search(/cadastro/g);
+        cy.log(dorme);
+        if(dorme != -1) {
+            cy.get("#nome").type(nome);
+            cy.get("#email").type(email);
+            cy.get("#senha").type(senha);
+            cy.get("input[value='Cadastrar']").click();
+
+            cy.contains("Novo usuário").click();
+        }
+        else {
+            cy.contains("Novo usuário").click();
+
+            cy.get("#nome").type(nome);
+            cy.get("#email").type(email);
+            cy.get("#senha").type(senha);
+            cy.get("input[value='Cadastrar']").click();
+        }
+        });
 });
